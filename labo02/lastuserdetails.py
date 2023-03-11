9c865e819b550e23214d6f8eee65ee0a7c5d37a1
Mounir Bouijdad <mounir.bouijdad@student.ap.be>
2023-03-11 19:12:45 +0100

    edit

--- labo02/lastuserdetails.py
+++ labo02/lastuserdetails.py
@@ -2,14 +2,15 @@
 
 with open('/etc/passwd', 'r') as passwd_file:
     last_line = passwd_file.readlines()[-1].strip()
+
     # split de lijn in velden op basis van de scheidingsteken ':'
     fields = last_line.split(':')
+
     # haal de gewenste velden op
     username = fields[0]
     uid = fields[2]
     home_dir = fields[5]
 
-# print de resultaten
 print(f"Gebruikersnaam: {username}")
 print(f"UserID: {uid}")
 print(f"Home directory: {home_dir}")
